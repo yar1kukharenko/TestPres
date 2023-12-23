e9fdd36c046f113fd4ba454f8d5ff1d1e3af1eac
Iaroslav Kukharenko <113698528+yar1kukharenko@users.noreply.github.com>
2023-12-23 12:25:25 +0300

    update animation

--- src/js/modules/animation.js
+++ src/js/modules/animation.js
@@ -5,19 +5,19 @@ export default function () {
   let currentStepProb = 0;
   let currentStepMicro = 0;
   const stepsBelok = [
-    { x: 0, y: 0, s: 1, z: 1 }, // Начальная позиция
-    { x: "-1100", y: "-300", s: 0.55, z: -1 }, // Позиция 1
-    { x: -230, y: -440, s: 0.45, z: -1 }, // Позиция 2
+    { x: 0, y: 0, s: 1, z: 1 },
+    { x: "-1100", y: "-300", s: 0.55, z: -1 },
+    { x: -230, y: -440, s: 0.45, z: -1 },
   ];
   const stepsProb = [
-    { x: 0, y: 0, s: 1, z: -1 }, // Начальная позиция
-    { x: 850, y: "-150", s: 0.7, z: -1 }, // Позиция 1
-    { x: 1200, y: 380, s: 1.8, z: 1 }, // Позиция 2
+    { x: 0, y: 0, s: 1, z: -1 },
+    { x: 850, y: "-150", s: 0.7, z: -1 },
+    { x: 1200, y: 380, s: 1.8, z: 1 },
   ];
   const stepsMicro = [
-    { x: 0, y: 0, s: 1, z: -1 }, // Начальная позиция
-    { x: 200, y: "500", s: 2, z: 1 }, // Позиция 1
-    { x: -700, y: 150, s: 1.5, z: -1 }, // Позиция 2
+    { x: 0, y: 0, s: 1, z: -1 },
+    { x: 200, y: "500", s: 2, z: 1 },
+    { x: -750, y: 150, s: 1.3, z: -1 },
   ];
 
   const belok = document.querySelector(".belok");
@@ -75,7 +75,28 @@ export default function () {
     });
   }
 
+  function setZIndex(direction) {
+    if (direction === "forward") {
+      belok.style.zIndex = stepsBelok[currentStepBelok].z;
+      prob.style.zIndex = stepsProb[currentStepProb].z;
+      micro.style.zIndex = stepsMicro[currentStepMicro].z;
+    } else {
+      // Для движения назад нужно учитывать предыдущий шаг
+      let previousStepBelok =
+        (currentStepBelok - 1 + stepsBelok.length) % stepsBelok.length;
+      let previousStepProb =
+        (currentStepProb - 1 + stepsProb.length) % stepsProb.length;
+      let previousStepMicro =
+        (currentStepMicro - 1 + stepsMicro.length) % stepsMicro.length;
+
+      belok.style.zIndex = stepsBelok[previousStepBelok].z;
+      prob.style.zIndex = stepsProb[previousStepProb].z;
+      micro.style.zIndex = stepsMicro[previousStepMicro].z;
+    }
+  }
+
   function updateStep(direction) {
+    setZIndex(direction);
     if (direction === "forward") {
       currentStepBelok = (currentStepBelok + 1) % stepsBelok.length;
       currentStepProb = (currentStepProb + 1) % stepsProb.length;
@@ -99,12 +120,31 @@ export default function () {
     startX = e.pageX;
   });
 
-  container.addEventListener("mouseup", function (e) {
-    const endX = e.pageX;
-    if (startX > endX) {
-      updateStep("backward");
-    } else {
-      updateStep("forward");
+  function handleStart(event) {
+    startX = event.touches ? event.touches[0].pageX : event.pageX;
+  }
+
+  const SWIPE_THRESHOLD = 50;
+
+  function handleEnd(event) {
+    let endX = event.changedTouches
+      ? event.changedTouches[0].pageX
+      : event.pageX;
+    let swipeLength = Math.abs(startX - endX);
+
+    if (swipeLength >= SWIPE_THRESHOLD) {
+      if (startX > endX) {
+        updateStep("backward");
+      } else {
+        updateStep("forward");
+      }
     }
-  });
+  }
+
+  container.addEventListener("mousedown", handleStart);
+  container.addEventListener("mouseup", handleEnd);
+
+  // Добавляем обработчики для событий касания
+  container.addEventListener("touchstart", handleStart);
+  container.addEventListener("touchend", handleEnd);
 }
